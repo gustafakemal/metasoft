275f3ebd3a6a344c494161ca6ecb8d6621011576
GOLDIEGUNADI <42563583+GOLDIEGUNADI@users.noreply.github.com>
2022-08-31 08:11:24 +0700

    Form Input Produk 31/8/22

--- app/Views/MFProduk/input.php
+++ app/Views/MFProduk/input.php
@@ -31,7 +31,7 @@
 
 <div class="dynamic-content">
 
-<div class="tbl-data-product">
+<div class="tbl-data-product show">
 
 <table id="dataList" class="table table-bordered table-striped" style="width: 100%;">
 	
@@ -123,8 +123,10 @@
 	<h5>Part Produk</h5>
     </div>
     <div class="col text-right">
-	<button type="button" class="btn btn-success">Cari</button>
-	<button type="button" class="btn btn-success">Tambah</button>
+	<button type="button" class="btn btn-success" data-toggle="modal" data-target="#cariPart">
+	<i class="fas fa-solid fa-search text-light"></i>&nbsp;Cari
+</button>
+	<a href="MFPartProduk/addPartProduct" type="button" class="btn btn-success"><i class="fa fa-plus-circle text-light"></i>&nbsp;Tambah</a>
     </div>
   </div>
  
@@ -159,7 +161,9 @@
 	<h6>Nama Part</h5>
     </div>
     <div class="col text-right">
-	<button type="button" class="btn btn-success">Tambah</button>
+	<button type="button" class="btn btn-success" data-toggle="modal" data-target="#dataFormSisi">
+	<i class="fa fa-plus-circle text-light"></i>&nbsp;Tambah Sisi
+</button>
     </div>
   </div>
  
@@ -170,7 +174,6 @@
 	<thead>
 		<tr>
 			<th>Sisi</th>
-			<th>FGD</th>
 			<th>Frontside</th>
 			<th>Backside</th>
 			<th>Special Requirements</th>
@@ -184,17 +187,17 @@
 </table>
 
 </div>
-<button type="button" class="btn btn-primary">Simpan</button>
 </div>
 
 <!-- Modal -->
-<div class="modal fade" id="dataForm" tabindex="-1" aria-labelledby=dataFormLabel" aria-hidden="true">
+
+<div class="modal fade modalForm" id="dataFormSisi" tabindex="-1" aria-labelledby=dataFormLabel" aria-hidden="true">
 	<div class="modal-dialog modal-lg">
 		<form>
 			<input type="hidden" name="id" value="" />
 			<div class="modal-content">
 				<div class="modal-header">
-					<h5 class="modal-title" id=dataFormLabel"></h5>
+					<h5 class="modal-title" id="dataFormLabel">Sisi Part Produk<br>Nama Part Produk</h5>
 					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 						<span aria-hidden="true">&times;</span>
 					</button>
@@ -202,32 +205,216 @@
 				<div class="modal-body">
 					<div class="msg"></div>
 					<div class="row">
-						<div class="col">
-							<div class="form-group">
-								<label for="nama">Jenis Flute <span class="text-danger">*</span></label>
-								<input name="nama" type="text" class="form-control" id="nama">
+						<label for="fgd" class="col-sm-2 col-form-label">No FGD</span></label>
+						<div class="col-sm-4">
+						<!-- <input type="text" class="form-control" id="tfgd" name="tfgd" disabled><input type="hidden" class="form-control" id="fgd" name="fgd"> -->
+						<input type="text" class="form-control" id="fgd" name="fgd" readonly>
+						</div>
+						<label for="trevisi" class="col-sm-2 col-form-label">Revisi</span></label>
+						<div class="col-sm-4">
+							<input type="text" class="form-control" id="trevisi" name="trevisi" disabled><input type="hidden" class="form-control" id="revisi" name="revisi">
+						</div>
+					</div>
+					<div class="row">
+						<label for="sisi" class="col-sm-2 col-form-label">Sisi</span></label>
+						<div class="col-sm">
+						<input type="text" class="form-control" id="sisi" name="sisi">
+						</div>
+						<label for="frontside" class="col-sm col-form-label">Frontside</span></label>
+						<div class="col-sm">
+						<input type="text" class="form-control" id="frontside" name="frontside">
+						</div>
+						<label for="backside" class="col-sm col-form-label">Backside</span></label>
+						<div class="col-sm">
+						<input type="text" class="form-control" id="backside" name="backside">
+						</div>
+					</div>
+					<div class="row">
+						<label for="special_req" class="col-sm-2 col-form-label">Special Requirement</span></label>
+						<div class="col-sm">
+						<textarea class="form-control" id="special_req" name="special_req"></textarea>
+						</div>
+						
+					</div>
+
+					<div class="mt-2">
+					<nav>
+					<div class="nav nav-tabs" id="nav-tab" role="tablist">
+						<button class="nav-link active" id="nav-warna-tab" data-toggle="tab" data-target="#nav-warna" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Warna</button>
+						<button class="nav-link" id="nav-proses-tab" data-toggle="tab" data-target="#nav-proses" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Proses</button>
+					</div>
+					</nav>
+
+					<div class="tab-content" id="nav-tabContent">
+						<div class="tab-pane fade show active" id="nav-warna" role="tabpanel" aria-labelledby="nav-warna-tab">
+
+						<div class="row mt-2">
+							<div class="col-sm-6">
+								<form>
+									<div class="form-group row">
+										<label for="tinta" class="col-sm-2  col-form-label">Frontside</label>
+										<div class="col-sm">
+											<select name="tinta" class="form-control" id="tinta">
+												<?php foreach ($opsi_jenistinta as $key => $opsi_jenistinta_item) : ?>
+													<option value="<?= $opsi_jenistinta_item->id;?>"><?= $opsi_jenistinta_item->nama;?></option>
+												<?php endforeach;?>
+											</select>
+										</div>
+										<div class="col-sm">
+											<button type="submit" class="btn-sm btn-primary"><i class="fas fa-solid fa-plus text-light"></i></button>
+										</div>
+									</div>
+									
+									</form>
+									<div class="row mb-1">
+										<label for="tinta" class="col-sm-2">&nbsp</label>
+										<div class="col-sm">CYAN</div>
+										<div class="col-sm">
+											<button type="submit" class="btn-sm btn-danger"><i class="fas fa-trash-alt text-light"></i></button>
+										</div>										
+									</div>
+									<div class="row mb-1">
+										<label for="tinta" class="col-sm-2">&nbsp</label>
+										<div class="col-sm">BLACK</div>	
+										<div class="col-sm">
+											<button type="submit" class="btn-sm btn-danger"><i class="fas fa-trash-alt text-light"></i></button>
+										</div>																
+									</div>
 							</div>
-							<div class="form-group">
-								<label for="harga">Harga <span class="text-danger">*</span></label>
-								<input name="harga" type="number" class="form-control" id="harga" value="0">
+							<div class="col-sm-6">
+								<form>
+									<div class="form-group row">
+										<label for="tinta" class="col-sm-2  col-form-label">Backside</label>
+										<div class="col-sm">
+											<select name="tinta" class="form-control" id="tinta">
+												<?php foreach ($opsi_jenistinta as $key => $opsi_jenistinta_item) : ?>
+													<option value="<?= $opsi_jenistinta_item->id;?>"><?= $opsi_jenistinta_item->nama;?></option>
+												<?php endforeach;?>
+											</select>
+										</div>
+										<div class="col-sm">
+											<button type="submit" class="btn-sm btn-primary"><i class="fas fa-solid fa-plus text-light"></i></button>
+										</div>
+									</div>
+									
+									</form>
+									<div class="row mb-1">
+										<label for="tinta" class="col-sm-2">&nbsp</label>
+										<div class="col-sm">CYAN</div>
+										<div class="col-sm">
+											<button type="submit" class="btn-sm btn-danger"><i class="fas fa-trash-alt text-light"></i></button>
+										</div>										
+									</div>
+									<div class="row">
+										<label for="tinta" class="col-sm-2">&nbsp</label>
+										<div class="col-sm">BLACK</div>	
+										<div class="col-sm">
+											<button type="submit" class="btn-sm btn-danger"><i class="fas fa-trash-alt text-light"></i></button>
+										</div>																
+									</div>
 							</div>
-							<div class="form-group">
-								<label for="aktif">Status</label>
-								<!-- <div class="form-check">
-									<input class="form-check-input" checked="checked" type="radio" name="aktif" id="msJenisFluteAktif" value="Y">
-									<label class="form-check-label" for="msJenisFluteAktif">Aktif</label>
-								</div>
-								<div class="form-check">
-									<input class="form-check-input" type="radio" name="aktif" id="msJenisFluteNonaktif" value="T">
-									<label class="form-check-label" for="msJenisFluteNonaktif">Nonaktif</label>
-								</div> -->
-								<select name="aktif" class="form-control" id="aktif">
-									<option value="Y">Aktif</option>
-									<option value="T">Nonaktif</option>
-								</select>
+						</div>
+
+						</div>
+						<div class="tab-pane fade" id="nav-proses" role="tabpanel" aria-labelledby="nav-proses-tab">
+						<div class="row mt-2">
+							<div class="col-sm-4">
+								<form>
+								<label for="tinta" class="form-label">Proses Manual</label>
+										<div class="form-group row">
+										<div class="col-sm">
+											<select name="tinta" class="form-control" id="tinta">
+												<?php foreach ($opsi_jenistinta as $key => $opsi_jenistinta_item) : ?>
+													<option value="<?= $opsi_jenistinta_item->id;?>"><?= $opsi_jenistinta_item->nama;?></option>
+												<?php endforeach;?>
+											</select>
+										</div>
+										<div class="col-sm">
+											<button type="submit" class="btn-sm btn-primary"><i class="fas fa-solid fa-plus text-light"></i></button>
+										</div>
+									</div>
+									
+									</form>
+									<div class="row mb-1">
+										<div class="col-sm">CYAN</div>
+										<div class="col-sm">
+											<button type="submit" class="btn-sm btn-danger"><i class="fas fa-trash-alt text-light"></i></button>
+										</div>										
+									</div>
+									<div class="row mb-1">
+										<div class="col-sm">BLACK</div>	
+										<div class="col-sm">
+											<button type="submit" class="btn-sm btn-danger"><i class="fas fa-trash-alt text-light"></i></button>
+										</div>																
+									</div>
+							</div>
+							<div class="col-sm-4">
+								<form>
+								<label for="tinta" class="form-label">Proses Finishing</label>
+										<div class="form-group row">
+										<div class="col-sm">
+											<select name="tinta" class="form-control" id="tinta">
+												<?php foreach ($opsi_jenistinta as $key => $opsi_jenistinta_item) : ?>
+													<option value="<?= $opsi_jenistinta_item->id;?>"><?= $opsi_jenistinta_item->nama;?></option>
+												<?php endforeach;?>
+											</select>
+										</div>
+										<div class="col-sm">
+											<button type="submit" class="btn-sm btn-primary"><i class="fas fa-solid fa-plus text-light"></i></button>
+										</div>
+									</div>
+									
+									</form>
+									<div class="row mb-1">
+										<div class="col-sm">CYAN</div>
+										<div class="col-sm">
+											<button type="submit" class="btn-sm btn-danger"><i class="fas fa-trash-alt text-light"></i></button>
+										</div>										
+									</div>
+									<div class="row mb-1">
+										<div class="col-sm">BLACK</div>	
+										<div class="col-sm">
+											<button type="submit" class="btn-sm btn-danger"><i class="fas fa-trash-alt text-light"></i></button>
+										</div>																
+									</div>
 							</div>
 							
+							<div class="col-sm-4">
+								<form>
+								<label for="tinta" class="form-label">Proses Khusus</label>
+										<div class="form-group row">
+										<div class="col-sm">
+											<select name="tinta" class="form-control" id="tinta">
+												<?php foreach ($opsi_jenistinta as $key => $opsi_jenistinta_item) : ?>
+													<option value="<?= $opsi_jenistinta_item->id;?>"><?= $opsi_jenistinta_item->nama;?></option>
+												<?php endforeach;?>
+											</select>
+										</div>
+										<div class="col-sm">
+											<button type="submit" class="btn-sm btn-primary"><i class="fas fa-solid fa-plus text-light"></i></button>
+										</div>
+									</div>
+									
+									</form>
+									<div class="row mb-1">
+										<div class="col-sm">CYAN</div>
+										<div class="col-sm">
+											<button type="submit" class="btn-sm btn-danger"><i class="fas fa-trash-alt text-light"></i></button>
+										</div>										
+									</div>
+									<div class="row mb-1">
+										<div class="col-sm">BLACK</div>	
+										<div class="col-sm">
+											<button type="submit" class="btn-sm btn-danger"><i class="fas fa-trash-alt text-light"></i></button>
+										</div>																
+									</div>
+							</div>
 						</div>
+
+
+
+						</div>
+					</div>
 					</div>
 				</div>
 				<div class="modal-footer">
@@ -243,48 +430,59 @@
 </div>
 
 <!-- Modal -->
-<div class="modal fade" id="dataDetail" tabindex="-1" aria-labelledby="dataDetailLabel" aria-hidden="true">
-	<div class="modal-dialog modal-lg">
+<div class="modal fade modalForm" id="cariPart" tabindex="-1" aria-labelledby="cariPartLabel" aria-hidden="true">
+	<div class="modal-dialog modal-xl">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title" id="dataDetailLabel">Data Detail</h5>
+				<h5 class="modal-title" id="cariPartLabel">Cari Part Produk</h5>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
 			</div>
 			<div class="modal-body">
-				<div class="row">
-					<div class="col">
-						<div class="item">
-							<label>Jenis Flute</label>
-							<div class="custDet nama"></div>
-						</div>
-						<div class="item">
-							<label>Harga</label>
-							<div class="custDet harga"></div>
-						</div>
-						<div class="item">
-							<label>Status</label>
-							<div class="custDet aktif"></div>
-						</div>
-						<div class="item">
-							<label>Dibuat pada</label>
-							<div class="custDet added"></div>
-						</div>
-						<div class="item">
-							<label>Dibuat oleh</label>
-							<div class="custDet added_by"></div>
-						</div>
-						<div class="item">
-							<label>Diupdate pada</label>
-							<div class="custDet updated"></div>
-						</div>
-						<div class="item">
-							<label>Diupdate oleh</label>
-							<div class="custDet updated_by"></div>
-						</div>
-					</div>
-				</div>
+			<form name="form-caripartproduk">
+  <div class="form-row align-items-center">
+    <div class="col-6">
+      <label class="sr-only" for="cariproduk">Cari Part Produk</label>
+      <input type="text" style="text-transform:uppercase" class="form-control mb-2" name="cariproduk" id="cariproduk" placeholder="Cari Produk Berdasarkan Nomor FGD atau Nama Part Produk">
+    </div>
+	<div class="col-auto">
+      <button type="submit" class="btn btn-primary mb-2">Cari</button>
+    </div>
+    <div class="col-auto">
+      <a type="button" class="btn btn-primary add-new mb-2" href="mfpartproduk/addPartProduct">Buat Baru</a>
+    </div>
+  </div>
+</form>
+
+<div class="dynamic-content">
+
+<div class="tbl-data-partproduct">
+
+<table id="dataList" class="table table-bordered table-striped" style="width: 100%;">
+	
+	<thead>
+		<tr>
+			<th>No</th>
+			<th>FGD</th>
+			<th>Revisi</th>
+			<th>Nama Part</th>
+			<th>Kertas</th>
+			<th>Flute</th>
+			<th>Ukuran</th>
+			<th>&nbsp;</th>
+		</tr>
+	</thead>
+</table>
+
+</div>
+
+
+
+
+
+
+</div>
 			</div>
 			<div class="modal-footer">
 				<button name="cancel" type="button" class="btn btn-outline-secondary" data-dismiss="modal">Close</button>

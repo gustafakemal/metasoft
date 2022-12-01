cd62937acf27820ce1481dd39eb5ec3397dfe8ce
Alfin Andri <alfin.andri@gmail.com>
2022-12-01 11:18:56 +0700

    Refactor mxprospect controller

--- app/Controllers/MXProspect.php
+++ app/Controllers/MXProspect.php
@@ -23,31 +23,16 @@ class MXProspect extends BaseController
         $this->breadcrumbs->add('Dashbor', '/');
         $this->breadcrumbs->add('MXProspect', '/');
 
-        $customers = (new \App\Models\CustomerModel())->getCustomers();
-        $jenisproduk = (new \App\Models\MXJenisProdukModel())->asObject()->findAll();
-        $segmen = (new \App\Models\MXSegmenModel())->asObject()->findAll();
-        $konten = (new \App\Models\MXKontenModel())->asObject()->findAll();
-        $material = (new \App\Models\MXMaterialModel())->asObject()->findAll();
-        $bagmaking = (new \App\Models\MXBagMakingModel())->asObject()->findAll();
-        $bottom = (new \App\Models\MXBottomModel())->asObject()->findAll();
-        $aksesori = (new \App\Models\MXAksesoriModel())->asObject()->findAll();
-        $areakirim = (new \App\Models\MXAreaKirimModel())->asObject()->findAll();
-
-        return view('Forms/MXProspect', [
+        $views = [
             'page_title' => 'MX Prospect',
             'breadcrumbs' => $this->breadcrumbs->render(),
             'main_menu' => (new \App\Libraries\Menu())->render(),
             'alternatif' => $this->model->getAlternatif(),
-            'customers' => $customers,
-            'jenisproduk' => $jenisproduk,
-            'segmen' => $segmen,
-            'konten' => $konten,
-            'material' => $material,
-            'bagmaking' => $bagmaking,
-            'bottom' => $bottom,
-            'aksesori' => $aksesori,
-            'areakirim' => $areakirim,
-        ]);
+        ];
+
+        $views = array_merge($views, $this->requiredFields());
+
+        return view('Forms/MXProspect', $views);
     }
 
     /**
@@ -57,24 +42,9 @@ class MXProspect extends BaseController
      */
     public function addProcess()
     {
-        $data_request = $this->request->getPost();
+        $data = $this->request->getPost();
 
-        $data_request['NoProspek'] = $this->model->noProspek();
-        $data_request['Alt'] = 1;
-
-        $data_request['NamaProduk'] = strtoupper($data_request['NamaProduk']);
-        $data_request['Tanggal'] = Time::now()->toDateTimeString();
-        $data_request['CreatedBy'] = session()->get('UserID');
-        $data_request['Tebal'] = floatval(str_replace(',', '', trim($data_request['Tebal'])));
-        $data_request['Panjang'] = floatval(str_replace(',', '', trim($data_request['Panjang'])));
-        $data_request['Lebar'] = floatval(str_replace(',', '', trim($data_request['Lebar'])));
-        $data_request['Pitch'] = floatval(str_replace(',', '', trim($data_request['Pitch'])));
-        $data_request['TebalMat1'] = floatval(str_replace(',', '', trim($data_request['TebalMat1'])));
-        $data_request['TebalMat2'] = floatval(str_replace(',', '', trim($data_request['TebalMat2'])));
-        $data_request['TebalMat3'] = floatval(str_replace(',', '', trim($data_request['TebalMat3'])));
-        $data_request['TebalMat4'] = floatval(str_replace(',', '', trim($data_request['TebalMat4'])));
-        $data_request['Toleransi'] = floatval(str_replace(',', '', trim($data_request['Toleransi'])));
-        $data_request['Kapasitas'] = floatval(str_replace(',', '', trim($data_request['Kapasitas'])));
+        $data_request = $this->transformDataRequest($data);
 
         /** Insert form isian ke DB */
         $insert_data = $this->model->insert($data_request, false);
@@ -125,10 +95,11 @@ class MXProspect extends BaseController
             foreach ($query->getResult() as $key => $row) {
 
                 $alt_confirm = "return confirm('Menambahkan alternatif')";
+                $del_confirm = "return confirm('Hapus prospek?')";
 
                 $edit = '<a title="Edit" data-toggle="tooltip" data-placement="left" class="btn btn-sm btn-success edit-rev-item mr-2" href="'. site_url('listprospek/edit/' . $row->NoProspek) .'" title="Edit"><i class="far fa-edit"></i></a> ';
                 $alt = '<a title="Tambah Alt" data-toggle="tooltip" data-placement="left" class="btn btn-sm btn-info alt-item" href="#" data-no-prospect="'. $row->NoProspek .'" onclick="' . $alt_confirm . '" title="Alt"><i class="far fa-clone"></i></a> ';
-                $hapus = '<a title="Hapus" data-toggle="tooltip" data-placement="left" class="btn btn-sm btn-danger" href="#" onclick=""><i class="far fa-trash-alt"></i></a>';
+                $hapus = '<a title="Hapus" data-toggle="tooltip" data-placement="left" class="btn btn-sm btn-danger" href="#" onclick="' . $del_confirm . '"><i class="far fa-trash-alt"></i></a>';
 
                 $results[] = [
                     $key + 1,
@@ -173,37 +144,6 @@ class MXProspect extends BaseController
         }
     }
 
-    private function apiResponse($query)
-    {
-        $results = [];
-        if($query->getNumRows() > 0) {
-            foreach ($query->getResult() as $key => $row) {
-
-                $alt_confirm = "return confirm('Menambahkan alternatif')";
-
-                $edit = '<a title="Edit" data-toggle="tooltip" data-placement="left" class="btn btn-sm btn-success edit-rev-item mr-2" href="#" title="Edit"><i class="far fa-edit"></i></a> ';
-                $alt = '<a title="Tambah Alt" data-toggle="tooltip" data-placement="left" class="btn btn-sm btn-info alt-item" href="#" data-no-prospect="'. $row->NoProspect .'" onclick="' . $alt_confirm . '" title="Alt"><i class="far fa-clone"></i></a> ';
-                $hapus = '<a title="Hapus" data-toggle="tooltip" data-placement="left" class="btn btn-sm btn-danger" href="#" onclick=""><i class="far fa-trash-alt"></i></a>';
-
-                $results[] = [
-                    $key + 1,
-                    $row->NoProspek,
-                    $row->Alt,
-                    $row->NamaProduk,
-                    $row->Pemesan,
-                    $row->Jumlah,
-                    $row->Area,
-                    $row->CreatedBy,
-                    $row->Catatan,
-                    '',
-                    $edit . $alt . $hapus
-                ];
-            }
-        }
-
-        return $results;
-    }
-
     public function edit($NoProspek)
     {
         $this->breadcrumbs->add('Dashbor', '/');
@@ -212,52 +152,25 @@ class MXProspect extends BaseController
         $query = $this->model->getByNoProspect($NoProspek);
         $qq = (new \App\Models\MXProspekAksesoriModel())->getByProspekAlt($NoProspek, $query->getResult()[0]->Alt);
 
-        $customers = (new \App\Models\CustomerModel())->getCustomers();
-        $jenisproduk = (new \App\Models\MXJenisProdukModel())->asObject()->findAll();
-        $segmen = (new \App\Models\MXSegmenModel())->asObject()->findAll();
-        $konten = (new \App\Models\MXKontenModel())->asObject()->findAll();
-        $material = (new \App\Models\MXMaterialModel())->asObject()->findAll();
-        $bagmaking = (new \App\Models\MXBagMakingModel())->asObject()->findAll();
-        $bottom = (new \App\Models\MXBottomModel())->asObject()->findAll();
-        $aksesori = (new \App\Models\MXAksesoriModel())->asObject()->findAll();
-        $areakirim = (new \App\Models\MXAreaKirimModel())->asObject()->findAll();
-
-        return view('Forms/MXProspect_edit', [
+        $views = [
             'page_title' => 'MX Prospect',
             'breadcrumbs' => $this->breadcrumbs->render(),
             'main_menu' => (new \App\Libraries\Menu())->render(),
             'alternatif' => $this->model->getAlternatif(),
-            'customers' => $customers,
-            'jenisproduk' => $jenisproduk,
-            'segmen' => $segmen,
-            'konten' => $konten,
-            'material' => $material,
-            'bagmaking' => $bagmaking,
-            'bottom' => $bottom,
-            'aksesori' => $aksesori,
-            'areakirim' => $areakirim,
             'data' => $query->getResult()[0],
             'prospek_aksesori' => $qq->getResult()
-        ]);
+        ];
+
+        $views = array_merge($views, $this->requiredFields());
+
+        return view('Forms/MXProspect_edit', $views);
     }
 
     public function editProcess()
     {
-        $data_request = $this->request->getPost();
+        $data = $this->request->getPost();
 
-        $data_request['NamaProduk'] = strtoupper($data_request['NamaProduk']);
-        $data_request['Updated'] = Time::now()->toDateTimeString();
-        $data_request['UpdatedBy'] = session()->get('UserID');
-        $data_request['Tebal'] = floatval(str_replace(',', '', trim($data_request['Tebal'])));
-        $data_request['Panjang'] = floatval(str_replace(',', '', trim($data_request['Panjang'])));
-        $data_request['Lebar'] = floatval(str_replace(',', '', trim($data_request['Lebar'])));
-        $data_request['Pitch'] = floatval(str_replace(',', '', trim($data_request['Pitch'])));
-        $data_request['TebalMat1'] = floatval(str_replace(',', '', trim($data_request['TebalMat1'])));
-        $data_request['TebalMat2'] = floatval(str_replace(',', '', trim($data_request['TebalMat2'])));
-        $data_request['TebalMat3'] = floatval(str_replace(',', '', trim($data_request['TebalMat3'])));
-        $data_request['TebalMat4'] = floatval(str_replace(',', '', trim($data_request['TebalMat4'])));
-        $data_request['Toleransi'] = floatval(str_replace(',', '', trim($data_request['Toleransi'])));
-        $data_request['Kapasitas'] = floatval(str_replace(',', '', trim($data_request['Kapasitas'])));
+        $data_request = $this->transformDataRequest($data, true);
 
         if ( $this->model->updateData($data_request, $data_request['NoProspek'], $data_request['Alt']) ) {
             if( array_key_exists('aksesori', $data_request) && count($data_request['aksesori']) > 0) {
@@ -280,5 +193,64 @@ class MXProspect extends BaseController
             return redirect()->back()
                 ->with('error', '<p>' . implode('</p><p>', $this->model->errors()) . '</p>');
         }
+    }
+
+    public function delete($NoProspek)
+    {
+        //$query = $this->model->
+    }
+
+    private function transformDataRequest(array $data_request, $updated = false)
+    {
+        $newarr = [];
+        foreach ($data_request as $key => $row) {
+            switch($key) {
+                case 'Tebal':
+                case 'Panjang':
+                case 'Lebar':
+                case 'Pitch':
+                case 'TebalMat1':
+                case 'TebalMat2':
+                case 'TebalMat3':
+                case 'TebalMat4':
+                case 'Toleransi':
+                case 'Kapasitas':
+                    $newarr[$key] = floatval(str_replace(',', '', trim($row)));
+                    break;
+                case 'NamaProduk':
+                    $newarr[$key] = strtoupper($row);
+                    break;
+                default:
+                    $newarr[$key] = $row;
+                    break;
+            }
+        }
+
+        if( $updated ) {
+            $newarr['Updated'] = Time::now()->toDateTimeString();
+            $newarr['UpdatedBy'] = session()->get('UserID');
+        } else {
+            $newarr['Tanggal'] = Time::now()->toDateTimeString();
+            $newarr['CreatedBy'] = session()->get('UserID');
+            $newarr['NoProspek'] = $this->model->noProspek();
+            $newarr['Alt'] = 1;
+        }
+
+        return $newarr;
+    }
+
+    private function requiredFields()
+    {
+        return [
+            'customers' => (new \App\Models\CustomerModel())->getCustomers(),
+            'jenisproduk' => (new \App\Models\MXJenisProdukModel())->asObject()->findAll(),
+            'segmen' => (new \App\Models\MXSegmenModel())->asObject()->findAll(),
+            'konten' => (new \App\Models\MXKontenModel())->asObject()->findAll(),
+            'material' => (new \App\Models\MXMaterialModel())->asObject()->findAll(),
+            'bagmaking' => (new \App\Models\MXBagMakingModel())->asObject()->findAll(),
+            'bottom' => (new \App\Models\MXBottomModel())->asObject()->findAll(),
+            'aksesori' => (new \App\Models\MXAksesoriModel())->asObject()->findAll(),
+            'areakirim' => (new \App\Models\MXAreaKirimModel())->asObject()->findAll(),
+        ];
     }
 }

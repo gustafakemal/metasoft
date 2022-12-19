cdcb5c21461d8c6ba52bcd49cac6dcee2d7f5ad7
Alfin Andri <alfin.andri@gmail.com>
2022-12-19 10:20:23 +0700

    Ubah navigation access data master metafold

--- app/Controllers/MFJenisKertas.php
+++ app/Controllers/MFJenisKertas.php
@@ -39,16 +39,31 @@ class MFJenisKertas extends BaseController
      */
     public function apiGetAll(): ResponseInterface
 	{
+        $access = $this->common->getAccess(uri_string(true));
+        $navigation = new \App\Libraries\Navigation();
+        $navigation->setAccess($access);
+
 		$query = $this->model->getMFJenisKertas();
 
 		$data = [];
 		foreach ($query as $key => $value) {
 
 			$CreateDate = (Time::parse($value->added))->toDateTimeString();
-			 
-			$detail = '<a class="btn btn-primary btn-sm item-detail mr-1" href="#" data-id="' . $value->id . '" title="Detail"><i class="far fa-file-alt"></i></a>';
-			$edit = '<a class="btn btn-success btn-sm item-edit mr-1" href="#" data-id="' . $value->id . '" data-nama="'.$value->nama.'" data-harga="'.$value->harga.'" data-berat="'.$value->berat.'" data-added="'.$CreateDate.'" data-aktif="'.$value->aktif.'|Y,T" title="Edit"><i class="far fa-edit"></i></a>';
-			$hapus = '<a class="btn btn-danger btn-sm" href="' . site_url('jeniskertas/delete/' . $value->id) . '" data-id="' . $value->id . '" onclick="return confirm(\'Apa Anda yakin menghapus data ini?\')" title="Hapus"><i class="fas fa-trash-alt"></i></a>';
+
+            $detail = $navigation->button('detail', [
+                'data-id' => $value->id,
+            ]);
+            $edit = $navigation->button('edit', [
+                'data-id' => $value->id,
+                'data-nama' => $value->nama,
+                'data-harga' => $value->harga,
+                'data-berat' => $value->berat,
+                'data-added' => $CreateDate,
+                'data-aktif' => $value->aktif . '|Y,T',
+            ]);
+            $hapus = $navigation->button('delete', [
+                'data-href' => site_url('jeniskertas/delete/' . $value->id),
+            ]);
 		
 			$data[] = [
 				$key + 1,
--- app/Controllers/MFProsesKhusus.php
+++ app/Controllers/MFProsesKhusus.php
@@ -39,17 +39,30 @@ class MFProsesKhusus extends BaseController
      */
     public function apiGetAll(): ResponseInterface
 	{
+        $access = $this->common->getAccess(uri_string(true));
+        $navigation = new \App\Libraries\Navigation();
+        $navigation->setAccess($access);
+
         $query = $this->model->getMFProsesKhusus();
 
 		$data = [];
 		foreach ($query as $key => $value) {
 			$CreateDate = (Time::parse($value->added))->toDateTimeString();
 
-			$detail = '<a class="btn btn-primary btn-sm item-detail mr-1" href="#" data-id="' . $value->id . '" title="Detail"><i class="far fa-file-alt"></i></a>';
-			$edit = '<a class="btn btn-success btn-sm item-edit mr-1" href="#" data-id="' . $value->id . '" data-proses="'.$value->proses.'" data-harga="'.$value->harga.'" data-added="'.$CreateDate.'" data-aktif="'.$value->aktif.'|Y,T" title="Edit"><i class="far fa-edit"></i></a>';
-			$hapus = '<a class="btn btn-danger btn-sm" href="' . site_url('proseskhusus/delete/' . $value->id) . '" data-id="' . $value->id . '" onclick="return confirm(\'Apa Anda yakin menghapus data ini?\')" title="Hapus"><i class="fas fa-trash-alt"></i></a>';
+            $detail = $navigation->button('detail', [
+                'data-id' => $value->id,
+            ]);
+            $edit = $navigation->button('edit', [
+                'data-id' => $value->id,
+                'data-proses' => $value->proses,
+                'data-harga' => $value->harga,
+                'data-added' => $CreateDate,
+                'data-aktif' => $value->aktif . '|Y,T',
+            ]);
+            $hapus = $navigation->button('delete', [
+                'data-href' => site_url('proseskhusus/delete/' . $value->id),
+            ]);
 		
-			$CreateDate = (Time::parse($value->added))->toDateTimeString();
 			$data[] = [
 				$key + 1,
 				$value->id,

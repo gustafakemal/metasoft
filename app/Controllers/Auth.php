38cf21e61c63c7eca16d011a6ac1874f657c5b07
Alfin Andri <alfin.andri@gmail.com>
2022-12-29 16:23:01 +0700

    Ubah login submit ke ajax

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 use App\Models\UsersModel;
 use CodeIgniter\HTTP\RedirectResponse;
+use CodeIgniter\HTTP\ResponseInterface;
 
 class Auth extends BaseController
 {
@@ -13,30 +14,54 @@ class Auth extends BaseController
 		$this->model = new UsersModel();
 	}
 
+    function decrypt($data, $key) {
+        return openssl_decrypt(base64_decode($data), 'aes-128-ecb', $key, OPENSSL_PKCS1_PADDING);
+    }
+
     /**
      * @return string
      */
     public function login(): string
     {
+
         return view('login');
     }
 
     /**
-     * @return RedirectResponse
+     * @return \CodeIgniter\HTTP\ResponseInterface
      */
-    public function verify(): RedirectResponse
+    public function verify(): ResponseInterface
     {
-        $username = $this->request->getPost('username');
-        $password = $this->request->getPost('password');
+//        $username = $this->request->getPost('username');
+//        $password = $this->request->getPost('password');
+
+        $data = $this->request->getPost();
 
         $auth = service('auth');
-        $result=$auth->login($username, $password);
+        $result = $auth->login($data['username'], $data['password']);
+
         if($result['isValid']) {
-            return redirect()->to('/');
+            $response = [
+                'success' => true,
+                'redirect' => '/',
+                'msg' => ''
+            ];
         } else {
-            return redirect()->back()
-                            ->with('error', $result['msg']);
+            $response = [
+                'success' => false,
+                'redirect' => '',
+                'msg' => $result['msg']
+            ];
         }
+
+        return $this->response->setJSON($response);
+
+//        if($result['isValid']) {
+//            return redirect()->to('/');
+//        } else {
+//            return redirect()->back()
+//                            ->with('error', $result['msg']);
+//        }
     }
 
     /**
